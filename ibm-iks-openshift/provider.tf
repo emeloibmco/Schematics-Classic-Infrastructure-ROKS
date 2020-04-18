4049dbe3b35adf9cc505f9adea1e6e3c099eae43
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-04-17 19:30:50 -0500

    Update provider.tf

--- ibm-iks-openshift/provider.tf
+++ ibm-iks-openshift/provider.tf
@@ -1,7 +1,3 @@
-iaas_classic_username = 2059386_jhoinerrojas@ibm.com
-iaas_classic_api_key = a47020190de8b75e3084a1c9ddf3a5016658ca82cffcd17f92060abc7ee81d0b
-ibmcloud_api_key = 8FynCmLa_2Xy0NXCWU1W6b3gMeeGDjW7ibLCPfBs5Tcy
-
 
 provider "ibm" {
   iaas_classic_username    = "${var.iaas_classic_username}"
