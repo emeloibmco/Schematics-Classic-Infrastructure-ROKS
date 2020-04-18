0e77c510f7d1595bee53660080dc8483fc5b6190
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-04-17 19:24:35 -0500

    Update provider.tf

--- ibm-iks-openshift/provider.tf
+++ ibm-iks-openshift/provider.tf
@@ -1,13 +1,13 @@
 variable "iaas_classic_username" {
-  description = 2059386_jhoinerrojas@ibm.com
+  description = "2059386_jhoinerrojas@ibm.com"
 }
 
 variable "iaas_classic_api_key" {
-  description = a47020190de8b75e3084a1c9ddf3a5016658ca82cffcd17f92060abc7ee81d0b
+  description = "a47020190de8b75e3084a1c9ddf3a5016658ca82cffcd17f92060abc7ee81d0b"
 }
 
 variable "ibmcloud_api_key" {
-  description = 8FynCmLa_2Xy0NXCWU1W6b3gMeeGDjW7ibLCPfBs5Tcy
+  description = "8FynCmLa_2Xy0NXCWU1W6b3gMeeGDjW7ibLCPfBs5Tcy"
 }
 
 provider "ibm" {
